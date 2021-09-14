5ff5fae1c024a6e6a8f28519edc178faee3e9953
ms881 <polak23123@wod.re>
2021-09-14 22:57:44 +0200

    another shit

---
+++ Solution1/SpecProj/Features/SomethingInteresting.feature
@@ -0,0 +1,8 @@
+﻿Feature: SomethingInteresting
+
+@mytag
+Scenario: Doing something causes something else
+	Given I am authenticated to application as 'admin'
+	When I open settings using menu
+	Then Settings are opened
+	And 'admin' settings are available
---
+++ Solution1/SpecProj/Steps/AdminSettings.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecProj.Steps
+{
+    [Binding]
+    public sealed class AdminSettings
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public AdminSettings(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"I am authenticated to application as '(.*)'")]
+        public void GivenIAmAuthenticatedToApplicationAs(string p0)
+        {
+            
+        }
+
+        [When(@"I open settings using menu")]
+        public void WhenIOpenSettingsUsingMenu()
+        {
+        }
+
+        [Then(@"Settings are opened")]
+        public void ThenSettingsAreOpened()
+        {
+        }
+
+        [Then(@"'(.*)' settings are available")]
+        public void ThenSettingsAreAvailable(string p0)
+        {
+            Assert.Pass();
+        }
+
+    }
+}
