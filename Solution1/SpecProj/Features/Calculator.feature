0c45ffe4ebbe828dba720e46923206808e8e004f
ms881 <polak23123@wod.re>
2021-09-14 21:47:59 +0200

    fesw

---
+++ Solution1/SpecProj/Features/Calculator.feature
@@ -0,0 +1,16 @@
+﻿Feature: Calculator
+
+@mytag
+Scenario Outline: Add two numbers
+	Given the first '<a>' number is '<b>'
+
+	Examples: 
+	| a | b  |
+	| 5 | 55 |
+	| 5 | 55 |
+	| 5 | 55 |
+	| 5 | 55 |
+	| 5 | 55 |
+	| 5 | 55 |
+	| 5 | 55 |
+	| 5 | 55 |
---
+++ Solution1/SpecProj/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace SpecProj.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"the first '(.*)' number is '(.*)'")]
+        public void GivenTheFirstNumberIs(int p0, int p1)
+        {
+            var expected = p0 + 50;
+
+            Assert.AreEqual(expected, p1);
+        }
+
+    }
+}
